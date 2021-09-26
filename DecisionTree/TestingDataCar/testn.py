b929b3fe8c8553f9d4ba135a713c8f6723260c16
Ben <kortosh@DESKTOP-IO2QSJ3.localdomain>
2021-09-26 12:50:13 -0600

    Removed an irrelevant file

--- DecisionTree/TestingDataCar/testn.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn.preprocessing import normalize
-
-arr = np.array([[2, 2, 2]])
-
-
-
-vec = normalize(arr, norm='l1')
-print(1 - np.dot(vec, np.transpose(vec))[0][0])
